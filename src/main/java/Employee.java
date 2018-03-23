88a386fe7b9a5f0ee9bc7982043456d2c60bb112
jskipp <jskipp@cs.odu.edu>
2018-03-23 00:40:56 -0400

    Added Documentation and methods for best pracitce

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -2,131 +2,226 @@ package main.java;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 
+/**
+ * The Employee class represents a salaried employee 
+ * belonging to some organization.
+ * 
+ * @author Jskipp
+ */
 public class Employee {
 	
 	/**
+	 * The ObjectId of the mongodb persistent
+	 * object representation.
 	 * 
+	 * @Type String
 	 */
 	@Id private String id;
 
 	/**
+	 * The first name of the employee
 	 * 
+	 * @Type String
 	 */
 	private String firstName;
 	
 	/**
+	 * The last name of the employee
 	 * 
+	 * @Type String
 	 */
 	private String lastName;
 	
 	/**
+	 * The salary of the employee
 	 * 
+	 * @Type int
 	 */
 	private int salary;
 	
 	/**
+	 * the date the employee was hired
 	 * 
+	 * @Type Date
 	 */
-	private Date hiredate;
+	private Date hireDate;
 
 	
 	/**
-	 * 
+	 * Default no-arg Constructor 
 	 */
 	public Employee() {
-		
+		this.firstName = "";
+		this.lastName = "";
+		this.salary = 0;
+		this.hireDate = new Date(0);
 	}
 	
 	/**
+	 * Constructor
 	 * 
-	 * @param name
-	 * @param salary
-	 * @param ssn
+	 * @param firstName String the first name of the employee
+	 * @param lastName String the last name of the employee
+	 * @param salary int the salary of the employee
+	 * @param hireDate Date the date the employee was hired
 	 */
-	public Employee(String firstName, String lastName, int salary, Date hiredate) {
+	public Employee(String firstName, String lastName, int salary, Date hireDate) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.salary = salary;
-		this.hiredate = hiredate;
+		this.hireDate = hireDate;
 	}
 	
+	/**
+	 * Getter for id
+	 * 
+	 * @return String id
+	 */
 	public String getId() {
 		return this.id;
 	}
 	
 	/**
+	 * Getter for firstName
 	 * 
-	 * @return
+	 * @return String firstName
 	 */
 	public String getFirstName() {
 		return this.firstName;
 	}
 	
 	/**
+	 * Setter for firstName
 	 * 
-	 * @param firstName
+	 * @param firstName String
 	 */
 	public void setName(String firstName) {
 		this.firstName = firstName;
 	}
 	
 	/**
+	 * Getter for salary
 	 * 
-	 * @return
+	 * @return int salary
 	 */
 	public int getSalary() {
 		return this.salary;
 	}
 	
 	/**
+	 * Setter for salary
 	 * 
-	 * @param salary
+	 * @param salary int
 	 */
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
 	
 	/**
+	 * Getter for lastName
 	 * 
-	 * @return
+	 * @return String lastName
 	 */
 	public String getLastName() {
 		return lastName;
 	}
 
 	/**
+	 * Setter for lastName
 	 * 
-	 * @param lastName
+	 * @param lastName String
 	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 	
-	public Date getHiredate() {
-		return hiredate;
+	/**
+	 * Getter for hireDate
+	 * 
+	 * @return Date hireDate
+	 */
+	public Date getHireDate() {
+		return hireDate;
 	}
 	
-	public void setHiredate(Date hiredate) {
-		this.hiredate = hiredate;
+	/**
+	 * Setter for hireDate
+	 * 
+	 * @param hireDate Date
+	 */
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
 	}
 
-	public void setHiredate(String hiredate) {
+	/**
+	 * Overloaded Setter for hireDate with a String
+	 * 
+	 * @param hireDate String the hireDate in yyyy-MM-dd format
+	 */
+	public void setHireDate(String hireDate) {
 		try {
-			this.hiredate = new SimpleDateFormat("yyyy-MM-dd").parse(hiredate);
+			this.hireDate = new SimpleDateFormat("yyyy-MM-dd").parse(hireDate);
 		} catch (Exception e) {
 			// Do nothing
 		}
 	}
 	
 	/**
+	 * Overridden toString method
 	 * 
+	 * @return String string representation of Employee
 	 */
 	@Override
 	public String toString() {
-		return "Employee " + id + " " + lastName + ", " 
-				+ firstName + ": Hired " + hiredate.toString();
+		return "Employee " + this.id + " " + 
+				"Name: " + this.lastName + ", " + this.firstName + ": " + 
+				"Hired: " + hireDate.toString() + " " +
+				"Salary: " + this.salary;
 	}
+	
+	/**
+	 * Override of the equals(Object) method inherited
+	 * from the Object class
+	 * 
+	 * @param obj Object the object to compare
+	 * @return boolean whether or not the objects have equality
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		// Check for self compare
+		if (obj == this) {
+			return true;
+		}
+		
+		// Check if obj is an instance of Employee
+		if (!(obj instanceof Employee)) {
+			return false;
+		}
+		
+		// Cast for comparisons
+		Employee emp = (Employee) obj;
+		
+		// Return field by field comparison
+		return this.firstName == emp.firstName &&
+				this.lastName == emp.lastName &&
+				this.hireDate == emp.hireDate &&
+				this.salary == emp.salary;
+	}
+	
+	/**
+	 * Override of the hashCode method inherited from the
+	 * Object class. Uses Objects class and fields to generate
+	 * a hash value.
+	 * 
+	 * @return int the hash
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(firstName, lastName, hireDate, salary);
+	}
+	
 }
